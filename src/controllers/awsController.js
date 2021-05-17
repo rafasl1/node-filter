07475496938b81d66a7be829b205613e84109985
Rafael Silva de Lima <rafaellima1100@gmail.com>
2021-05-17 13:32:27 -0300

    add property filter code

--- src/controllers/awsController.js
+++ src/controllers/awsController.js
@@ -1,45 +1,67 @@
 const AwsController = {
     async index(request, response) {
+        const { fields } = request.query
+        const separatedFields = fields.split("-") 
+
         const instances = [
             {
                 "id": 1,
                 "resourceType": "Redshift Cluster",
                 "status": "Running",
-                "Team": "Winterhold",
-                "Slack": "#winterhold"
+                "team": "Winterhold",
+                "slack": "#winterhold"
             },
             {
                 "id": 2,
                 "resourceType": "DynamoDB Table",
                 "status": "Running",
-                "Team": "Tulum",
-                "Slack": "#tulum"
+                "team": "Tulum",
+                "slack": "#tulum"
             },
             {
                 "id": 3,
                 "resourceType": "S3 bucket",
                 "status": "Sleeping",
-                "Team": "Winterhold",
-                "Slack": "#winterhold"
+                "team": "Winterhold",
+                "slack": "#winterhold"
             },
             {
                 "id": 4,
                 "resourceType": "Redshift Cluster",
                 "status": "Running",
-                "Team": "Winterfell",
-                "Slack": "#winterfell"
+                "team": "Winterfell",
+                "slack": "#winterfell"
             },
             {
                 "id": 5,
                 "resourceType": "EC2 Instance",
                 "status": "Running",
-                "Team": "Condado",
-                "Slack": "#condado"
+                "team": "Condado",
+                "slack": "#condado"
+            },
+            { 
+                "fields": separatedFields
             }
         ]
 
+
+        for(let i = 0; i < instances.length; i++){
+            Object.keys(instances[i]).forEach(key => {
+                let deleteKey = true
+
+                separatedFields.forEach(field => {
+                    if(key == field) deleteKey = false
+                })
+
+                if(deleteKey) delete (instances[i])[key]
+            });
+        }
+
         return response.json(instances)
+    
     }
+    
+    
 }
 
 export default AwsController
